03c4de7d26a8b55736328860b271b93b0a06d1a9
Matthieu MINET <m.minet@tf1.onmicrosoft.com>
2021-08-06 14:07:59 +0200

    Update output values

--- output.tf
+++ output.tf
@@ -1,9 +1,14 @@
-output "resource_group_id" {
+output "rg_id" {
   description = "Resource Group ID"
   value       = azurerm_resource_group.az_rg.id
 }
 
-output "resource_group_name" {
+output "rg_name" {
   description = "Resource Group name"
   value       = azurerm_resource_group.az_rg.name
+}
+
+output "rg_location" {
+  description = "Resource Group location"
+  value       = azurerm_resource_group.az_rg.location
 }
